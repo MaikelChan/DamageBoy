9f5f09459f1aed3d9d09f98c592fc53cb4fa64bc
MaikelChan <maikelchan88@gmail.com>
2024-03-05 05:51:05 +0100

    Timers rewritten. Now passes more tests.

--- DamageBoy.Core/Timer.cs
+++ DamageBoy.Core/Timer.cs
@@ -5,107 +5,116 @@ namespace DamageBoy.Core;
 
 class Timer : IState
 {
+    readonly GameBoy gameBoy;
     readonly InterruptHandler interruptHandler;
 
     public enum TimerClockSpeeds : byte { Hz4096, Hz262144, Hz65536, Hz16384 }
 
-    public byte Divider { get; set; }
+    public byte Divider => (byte)(currentSystemCounter >> 8);
     public bool TimerEnable { get; set; }
     public TimerClockSpeeds TimerClockSpeed { get; set; }
-    public byte TimerCounter { get; set; }
-    public byte TimerModulo { get; set; }
 
-    int dividerClocksToWait;
-    int timerClocksToWait;
-
-    bool timerHasOverflown;
-    int timerOverflowWaitCycles;
-
-    const int DIV_FREQUENCY = 256;
-
-    public Timer(InterruptHandler interruptHandler)
+    byte timerCounter;
+    public byte TimerCounter
     {
-        this.interruptHandler = interruptHandler;
+        get => timerCounter;
+        set
+        {
+            if (timerOverflowWaitCycles == 4)
+                return;
+            if (timerOverflowWaitCycles == 8)
+            {
+                interruptHandler.RequestTimerOverflow = false;
+                timerOverflowWaitCycles = 0;
+            }
+            timerCounter = value;
+        }
+    }
 
-        dividerClocksToWait = DIV_FREQUENCY; // Should be 12 * 4 if loading without boot ROM
-        timerClocksToWait = GetTimerClocks();
+    byte timerModulo;
+    public byte TimerModulo
+    {
+        get => timerModulo;
+        set
+        {
+            if (timerOverflowWaitCycles == 4)
+                timerCounter = value;
+            timerModulo = value;
+        }
+    }
+
+    bool TimerHasOverflown => timerOverflowWaitCycles > 0;
+
+    byte timerOverflowWaitCycles;
+    ushort currentSystemCounter;
+    bool previousSelectedBit;
+
+    public Timer(GameBoy gameBoy, InterruptHandler interruptHandler)
+    {
+        this.gameBoy = gameBoy;
+        this.interruptHandler = interruptHandler;
     }
 
     public void Update()
     {
-        dividerClocksToWait -= 4;
-        if (dividerClocksToWait <= 0)
+        SetSystemCounter((ushort)(currentSystemCounter + 4));
+        if (!TimerHasOverflown) return;
+
+        timerOverflowWaitCycles -= 4;
+
+        if (timerOverflowWaitCycles == 8)
         {
-            dividerClocksToWait = DIV_FREQUENCY;
-            Divider++;
-            //Utils.Log($"Divider Increase: {Divider}");
+            interruptHandler.RequestTimerOverflow = true;
         }
-
-        if (TimerEnable)
+        else if (timerOverflowWaitCycles == 4)
         {
-            timerClocksToWait -= 4;
-            if (timerClocksToWait <= 0)
-            {
-                timerClocksToWait = GetTimerClocks();
-
-                TimerCounter++;
-                if (TimerCounter == 0)
-                {
-                    timerOverflowWaitCycles = 8;
-                    timerHasOverflown = true;
-                }
-            }
-        }
-
-        if (timerHasOverflown)
-        {
-            timerOverflowWaitCycles -= 4;
-            if (timerOverflowWaitCycles <= 0)
-            {
-                timerHasOverflown = false;
-
-                TimerCounter = TimerModulo;
-                interruptHandler.RequestTimerOverflow = true;
-            }
+            timerCounter = timerModulo;
         }
     }
 
     public void ResetDIV()
     {
-        Divider = 0;
-        dividerClocksToWait = DIV_FREQUENCY;
-
-        timerClocksToWait = GetTimerClocks();
+        SetSystemCounter(0);
     }
 
-    int GetTimerClocks()
+    void SetSystemCounter(ushort value)
+    {
+        currentSystemCounter = value;
+
+        byte bitPosition = GetSelectedBitPosition();
+        bool currentSelectedBit = (currentSystemCounter & (1 << bitPosition)) != 0;
+        if (gameBoy.HardwareType == HardwareTypes.DMG) currentSelectedBit &= TimerEnable;
+
+        if (!currentSelectedBit && previousSelectedBit)
+        {
+            timerCounter++;
+            if (timerCounter == 0) timerOverflowWaitCycles = 12;
+        }
+
+        previousSelectedBit = currentSelectedBit;
+    }
+
+    byte GetSelectedBitPosition()
     {
         switch (TimerClockSpeed)
         {
             default:
-            case TimerClockSpeeds.Hz4096:
-                return timerClocksToWait = CPU.CPU_CLOCKS / 4096;
-            case TimerClockSpeeds.Hz262144:
-                return timerClocksToWait = CPU.CPU_CLOCKS / 262144;
-            case TimerClockSpeeds.Hz65536:
-                return timerClocksToWait = CPU.CPU_CLOCKS / 65536;
-            case TimerClockSpeeds.Hz16384:
-                return timerClocksToWait = CPU.CPU_CLOCKS / 16384;
+            case TimerClockSpeeds.Hz4096: return 9;
+            case TimerClockSpeeds.Hz262144: return 3;
+            case TimerClockSpeeds.Hz65536: return 5;
+            case TimerClockSpeeds.Hz16384: return 7;
         }
     }
 
     public void SaveOrLoadState(Stream stream, BinaryWriter bw, BinaryReader br, bool save)
     {
-        Divider = SaveState.SaveLoadValue(bw, br, save, Divider);
         TimerEnable = SaveState.SaveLoadValue(bw, br, save, TimerEnable);
         TimerClockSpeed = (TimerClockSpeeds)SaveState.SaveLoadValue(bw, br, save, (byte)TimerClockSpeed);
-        TimerCounter = SaveState.SaveLoadValue(bw, br, save, TimerCounter);
-        TimerModulo = SaveState.SaveLoadValue(bw, br, save, TimerModulo);
+        timerCounter = SaveState.SaveLoadValue(bw, br, save, timerCounter);
+        timerModulo = SaveState.SaveLoadValue(bw, br, save, timerModulo);
 
-        dividerClocksToWait = SaveState.SaveLoadValue(bw, br, save, dividerClocksToWait);
-        timerClocksToWait = SaveState.SaveLoadValue(bw, br, save, timerClocksToWait);
-
-        timerHasOverflown = SaveState.SaveLoadValue(bw, br, save, timerHasOverflown);
         timerOverflowWaitCycles = SaveState.SaveLoadValue(bw, br, save, timerOverflowWaitCycles);
+        currentSystemCounter = SaveState.SaveLoadValue(bw, br, save, currentSystemCounter);
+        previousSelectedBit = SaveState.SaveLoadValue(bw, br, save, previousSelectedBit);
     }
 }
